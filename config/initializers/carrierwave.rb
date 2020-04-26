16e0bc15412e04f1ff7ff7f6e046876103d9a0a0
Nipun Paradkar <nipunparadkar123@gmail.com>
2020-04-26 14:01:27 +0530

    Create carrierwave config

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_public = true
+  config.fog_directory = 'storage'
+
+  if Rails.env.development? || Rails.env.test?
+    config.fog_credentials = {
+      provider: 'Local',
+      local_root: "#{Rails.root}"
+    }
+  end
+end
